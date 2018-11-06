33af37f527a0ee8c6730520ca81e8c5396c36134
Your Name <you@example.com>
2018-11-06 10:45:42 -0500

    Lab21

---
+++ CoffeeShop/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using CoffeeShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CoffeeShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.index = "Welcome to the Grand Circus Coffee Shop";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Register()
+        {
+            ViewBag.Message = "Registration";
+
+            return View();
+        }
+        public ActionResult Result(Registration r)
+        {
+            ViewBag.name = r.prefix + " " + r.firstName + " " + r.lastName;
+
+            return View();
+        }
+    }
+}
---
+++ CoffeeShop/Models/Registration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CoffeeShop.Models
+{
+    public class Registration
+    {
+        public string prefix { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+        public string phoneNumber { get; set; }
+        public string streetNum { get; set; }
+        public string streetName { get; set; }
+        public string zipCode { get; set; }
+        public string state { get; set; }
+        public string password { get; set; }
+        public string dob { get; set; }
+    }
+}
